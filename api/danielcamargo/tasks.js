eab35ca41f8f5a1969a49264897883b8205d5e66
Daniel Teleginski Camargo <daniel.camargo@ensign.edu>
2023-05-02 08:51:51 -0600

    rendering the tasks table We are now able to see all the tasks from Daniel Camargo on a table

--- api/danielcamargo/tasks.js
+++ api/danielcamargo/tasks.js
@@ -6,16 +6,30 @@ const router = express.Router();
 /** @type{{id: number, name: string, done: boolean}[]} */
 const tasks = [
     { id: 1, name: "some name 1", done: false },
-    { id: 2, name: "some name 2", done: false },
+    { id: 2, name: "some name 2", done: true },
     { id: 3, name: "some name 3", done: false },
     { id: 4, name: "some name 4", done: false },
 ];
 
-// Search
+// GET /tasks
 router.get('/', function (req, res) {
-    console.log("Handling request to search tasks");
+    console.log("Handling request to list all tasks");
     // Return all tasks
     res.send(tasks);
+});
+
+// GET /tasks/5
+router.get('/:id', function (req, res) {
+    console.log("Handling request to find a task by id: ", req.params.id);
+    const id = parseInt(req.params.id);
+    const filteredTasks = tasks.filter((task) => task.id === id);
+    if (filteredTasks.length === 0) {
+        res.status(404).send({ message: "Not found" });
+        return;
+    }
+
+    // Return the task
+    res.send(filteredTasks[0]);
 });
 
 module.exports = router;
