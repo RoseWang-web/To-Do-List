9609475699cb6f49acd78c763b5149b7d8370679
Stefany Peixoto <Stefany Peixoto>
2023-05-01 19:25:37 -0600

    my api

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use('/api/danielcamargo/tasks', danielCamargoTasksRouter);
 const moroniTasksRouter = require('./api/moroniIbarra/tasks');
 app.use('/api/moroniIbarra/tasks', moroniTasksRouter);
 
+const stefanyTasksRouter = require('./api/stefany_peixoto/tasks');
+app.use('/api/stefany_peixoto/tasks', stefanyTasksRouter);
+
 // Start server
 app.listen(3000, () => {
     console.log('Server Started');
