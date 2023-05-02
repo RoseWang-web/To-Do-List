ef5df412f6beb6d0b8df6a524c25334606b6d2ec
brunadumer <brumenoncin@gmail.com>
2023-05-01 18:48:36 -0600

    adding Bruna Dumer api to server

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 // Register tasks router
+const brunaDumerTasksRouter = require('./api/brunaDumer/tasks');
+app.use('/api/brunaDumer/tasks', brunaDumerTasksRouter);
+
 const danielCamargoTasksRouter = require('./api/danielcamargo/tasks');
 app.use('/api/danielcamargo/tasks', danielCamargoTasksRouter);
 
