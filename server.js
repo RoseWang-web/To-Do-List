ebd179d0340bc4b971968546ae58c0c7fc493d0b
Rose <zxcasdqwe8898@gmail.com>
2023-05-02 15:42:52 -0600

    update

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use('/api/danielcamargo/tasks', danielCamargoTasksRouter);
 const moroniTasksRouter = require('./api/moroniIbarra/tasks');
 app.use('/api/moroniIbarra/tasks', moroniTasksRouter);
 
+const RoseTasksRouter = require('./api/Rose/tasks');
+app.use('/api/Rose/tasks', RoseTasksRouter);
+
 // Start server
 app.listen(3000, () => {
     console.log('Server Started');
