b1086681157ee2bdcbeb6927692770a9ce254805
yago muniz <yagomunizpinto@gmail.com>
2023-05-01 19:18:19 -0600

    Adding an endpoint

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use('/api/danielcamargo/tasks', danielCamargoTasksRouter);
 const moroniTasksRouter = require('./api/moroniIbarra/tasks');
 app.use('/api/moroniIbarra/tasks', moroniTasksRouter);
 
+const yagoTasksRouter = require('./api/yago/tasks');
+app.use('/api/yago/tasks', yagoTasksRouter);
+
 // Start server
 app.listen(3000, () => {
     console.log('Server Started');
