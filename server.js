b693cf86299721e6a663d745e28a4b6baa78f09d
Pedro Perillo <pedrofperillo@gmail.com>
2023-05-01 19:18:02 -0600

    We made an alteration, adding an endpoint to get by id

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use('/api/danielcamargo/tasks', danielCamargoTasksRouter);
 const moroniTasksRouter = require('./api/moroniIbarra/tasks');
 app.use('/api/moroniIbarra/tasks', moroniTasksRouter);
 
+const pedroTasksRouter = require('./api/pedroPerillo/tasks');
+app.use('/api/pedroPerillo/tasks', pedroTasksRouter);
+
 // Start server
 app.listen(3000, () => {
     console.log('Server Started');
