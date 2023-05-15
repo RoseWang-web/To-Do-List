a3c312708299e65a8f2e368131f95ad3c2555c54
Daniel Teleginski Camargo <daniel.camargo@ensign.edu>
2023-05-15 17:59:44 -0600

    fix typo

--- api/danielcamargo/tasks.js
+++ api/danielcamargo/tasks.js
@@ -6,9 +6,10 @@ const router = express.Router();
 /** @type{{id: number, name: string, done: boolean}[]} */
 const tasks = [
     { id: 1, name: "some name 1", done: false },
-    { id: 2, name: "some name 2", done: true },
+    { id: 2, name: "some name 2", done: false },
     { id: 3, name: "some name 3", done: false },
     { id: 4, name: "some name 4", done: false },
+    { id: 5, name: "some name 5", done: false },
 ];
 
 // GET /tasks
--- api/piero/tasks.js
+++ api/piero/tasks.js
@@ -2,8 +2,6 @@
 // https://expressjs.com/en/guide/routing.html
 const express = require('express');
 const router = express.Router();
-const pieroTinocoTaskrouter = require('./api/pierotinoco/tasks')
-
 
 /** @type{{id: number, name: string, done: boolean}[]} */
 const tasks = [{ id: 1, name: "some name", done: false }];
