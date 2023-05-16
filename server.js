a48d329509e3c00cf5512a0d05fe996ea019c429
Doyoung Yoon <qklzd13@gmail.com>
2023-05-15 23:51:59 -0600

    add Doyoung script

--- server.js
+++ server.js
@@ -33,9 +33,6 @@ app.use('/api/daniel/tasks', danielTasksRouter);
 
 const pedroTasksRouter = require('./api/pedroPerillo/tasks');
 app.use('/api/pedroPerillo/tasks', pedroTasksRouter);
-const doyoungTasksRouter = require('./api/doyoung/tasks');
-app.use('/api/doyoung/tasks', doyoungTasksRouter);
-
 
 const doyoungTasksRouter = require('./api/doyoung/tasks');
 app.use('/api/doyoung/tasks', doyoungTasksRouter);
