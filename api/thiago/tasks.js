553607139163c53be4c24c97a890a5a168ac0b30
Farias, Thiago <thiagomarsal.farias@gmail.com>
2023-05-17 10:02:16 -0600

    [Thiago] - An Amazing To-Do App - Part III

--- api/thiago/tasks.js
+++ api/thiago/tasks.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 /** @type{{id: number, name: string, done: boolean}[]} */
 const tasks = [
-    { id: 1, name: "Week 1", done: false },
+    { id: 1, name: "Week 1", done: true },
     { id: 2, name: "Week 2", done: false },
     { id: 3, name: "Week 3", done: false },
     { id: 4, name: "Week 4", done: false },
+    { id: 5, name: "Week 5", done: false },
 ];
 
 // GET /api/thiago/tasks/
@@ -19,15 +20,16 @@ router.get('/', function (req, res) {
 
 // GET /api/thiago/tasks/id={id}
 router.get('/:id', function (req, res) {
-    console.log("Handling request to search tasks");
+    console.log("Handling request to find a task by id: ", req.params.id);
     const id = parseInt(req.params.id);
-    const result = tasks.filter((task) => task.id === id);
-    if (result.length === 0){
-        res.status(404).send({ message: "Not found"});
+    const filteredTasks = tasks.filter((task) => task.id === id);
+    if (filteredTasks.length === 0) {
+        res.status(404).send({ message: "Not found" });
         return;
     }
-    // Return all tasks
-    res.send(result[0]);
+
+    // Return task
+    res.send(filteredTasks[0]);
 });
 
 module.exports = router;
