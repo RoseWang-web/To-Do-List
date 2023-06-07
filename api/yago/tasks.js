cdf083065ddd950bd53ae01202e51c966245013c
Daniel Teleginski Camargo <daniel.camargo@ensign.edu>
2023-06-07 09:16:59 -0600

    merging yago's branch manually

--- api/yago/tasks.js
+++ api/yago/tasks.js
@@ -9,90 +9,80 @@ const tasks = [
     { id: 2, name: "some name 2", done: false },
     { id: 3, name: "some name 3", done: false },
     { id: 4, name: "some name 4", done: false },
-    { id: 5, name: "some name 5", done: true },
 ];
 
-// Search
 router.get('/', function (req, res) {
     console.log("Handling request to search tasks");
-    // Return all tasks
     res.send(tasks);
 });
-// /tasks/1
+
+router.put('/:id', function (req, res) {
+    console.log("Handling request to update a task");
+    const id = parseInt(req.params.id);
+    const index = tasks.findIndex((task) => task.id === id);
+    if (index === -1) {
+        res.status(404).send({ message: "Not found" });
+        return;
+    }
+    const { done } = req.body;
+    if (typeof done !== "boolean") {
+        res.status(400).send({ message: "Invalid task data" });
+        return;
+    }
+    tasks[index].done = done;
+    res.send(tasks[index]);
+});
+// Search
 router.get('/:id', function (req, res) {
     console.log("Handling request to search tasks");
     const id = parseInt(req.params.id);
-    const result = tasks.filter((task) => task.id === id);
-    if (result.length === 0){
-        res.status(404).send({ message: "Not found"});
+    const filteredTasks = tasks.find((task) => task.id === id);
+    if (filteredTasks.length === 0) {
+        res.status(404).send({ message: "Not found" });
         return;
     }
     // Return all tasks
-    res.send(result[0]);
+
+    res.send(filteredTasks);
 });
 
-module.exports = router;
-/////////////////////////////////////////
-// Create a new task
+router.delete('/:id', function (req, res) {
+    console.log("Handling request to search tasks");
+    const id = parseInt(req.params.id);
+    const filteredTasks = tasks.findIndex((task) => task.id === id);
+    if (filteredTasks.length === -1) {
+        res.status(404).send({ message: "Not found" });
+        return;
+    }
+    // Remove task
+    tasks.splice(filteredTasks, 1)
+    res.status(204).send();
+});
+
 router.post('/', function (req, res) {
     console.log("Handling request to create a task");
-  
+
     const { name, done } = req.body;
-  
+
     // Input validation
     if (!name || typeof done !== "boolean") {
         res.status(400).send({ message: "Invalid task data" });
         return;
     }
-  
+    const date = new Date();
+    const number = date.getTime();
     // Create a new task object
     const newTask = {
-        id: (new Date()).getTime(),
+
+        id: number, // Generate a unique ID based on the length of the tasks array
         name: name,
         done: done
     };
-  
+
     // Add the new task to the tasks array
     tasks.push(newTask);
-  
-    // Respond with the new task
+
     res.status(201).send(newTask);
 });
 
-router.delete('/:id', function (req, res) {
-    console.log("Handling request to delete a task");
-    const id = parseInt(req.params.id);
-    const taskIndex = tasks.findIndex((task) => task.id === id);
-    if (taskIndex === -1) {
-        res.status(404).send({ message: "Not found" });
-        return;
-    }
-    // Remove task
-    tasks.splice(taskIndex, 1)
-    res.status(204).send();
-});
-
-// Update an existing task
-router.put('/:id', function (req, res) {
-    console.log("Handling request to update a task");
-
-    const id = parseInt(req.params.id);
-    const { done } = req.body;
-
-    // Input validation
-    if (typeof done !== "boolean") {
-        res.status(400).send({ message: "Invalid task data" });
-        return;
-    }
-
-    // Find the task
-    const task = tasks.find(task => task.id === id);
-
-    // Update the task
-    if (task) {
-        task.done = done;
-        res.send(task);
-    } else {
-        res.status(404).send({ message: "Task not found" });
-    }
-});
+module.exports = router;
