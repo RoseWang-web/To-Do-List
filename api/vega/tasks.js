2d50adf6b7df8dc43767c5d7743669395e439c0d
Jose Vega <jav410@ensign.edu>
2023-06-21 19:19:18 -0600

    wip

--- api/vega/tasks.js
+++ api/vega/tasks.js
@@ -3,45 +3,44 @@
 const express = require('express');
 const router = express.Router();
 
-/** @type{{id: number, name: string, done: boolean}[]} */
-const tasks = [
-    { id: 1, name: "some name 1", done: false },
-    { id: 2, name: "some name 2", done: false },
-    { id: 3, name: "some name 3", done: false },
-    { id: 4, name: "some name 4", done: false },
-];
+const mysqlQuery = require('./db');
 
 // GET /tasks
-router.get('/', function (req, res) {
+router.get('/', async function (req, res) {
     console.log("Handling request to search tasks");
-    // Return all tasks
+    const tasks = await mysqlQuery('select * from tasks');
     res.send(tasks);
 });
 
-// GET /tasks/:id
-router.get('/:id', function (req, res) {
+router.get('/wireframe/', function (req, res) {
     console.log("Handling request to search tasks");
-    const id = parseInt(req.params.id);
-    const targetTask = tasks.find((tasks) => tasks.id === id);
-    if (!targetTask) {
-        res.status(404).send({ message: "Task not found" });
+    // Return all tasks
+    // res.send(tasks);
+});
+
+// GET /tasks/:id
+router.get('/:id', async function (req, res) {
+    console.log("Handling request to search tasks");
+    const result = await mysqlQuery(`select * from tasks where id = ${req.params.id}`);    
+    if (!result.length){
+        res.status(404).send({message: "Task not found"});
         return;
     }
+
     // Return the task
-    res.send(targetTask);
+    res.send(result[0]);
 });
 
 // DELETE /tasks/:id
-router.delete('/:id', function (req, res) {
+router.delete('/:id', async function (req, res) {
     console.log("Deleting task");
-    const id = parseInt(req.params.id);
-    const targetIndex = tasks.findIndex((tasks) => tasks.id === id);
-    if (targetIndex === -1) { // -1 means not found
-        res.status(404).send({ message: "Task not found"});
+    const result = await mysqlQuery(`select * from tasks where id = ${req.params.id}`);
+    if (!result.length){
+        res.status(404).send({message: "Task not found"});
         return;
     }
-    // Remove the task
-    tasks.splice(targetIndex, 1);
+
+    await mysqlQuery(`delete from tasks where id = ${req.params.id}`);
     // Return the task
     res.status(204).send();
 });
@@ -51,7 +50,7 @@ router.post('/', function (req, res) {
     console.log("Creating task");
     const task = req.body;
     if (!task.name) {
-        res.status(400).send({ message: "Task must have a name" });
+        res.status(400).send({message: "Task must have a name"});
         return;
     }
     // Add the task
@@ -61,17 +60,18 @@ router.post('/', function (req, res) {
 });
 
 // PUT /tasks/:id
-router.put('/:id', function (req, res) {
+router.put('/:id', async function (req, res) {
     console.log("Updating task");
+    const result = await mysqlQuery(`select * from tasks where id = ${req.params.id}`);   
     const id = parseInt(req.params.id);
     const task = req.body;
     if (!task.name) {
-        res.status(400).send({ message: "Task must have a name" });
+        res.status(400).send({message: "Task must have a name"});
         return;
     }
     const targetIndex = tasks.findIndex((tasks) => tasks.id === id);
     if (targetIndex === -1) { // -1 means not found
-        res.status(404).send({ message: "Task not found" });
+        res.status(404).send({message: "Task not found"});
         return;
     }
     // Update the task
@@ -80,14 +80,11 @@ router.put('/:id', function (req, res) {
 });
 
 //DELETE /allTasks
-router.delete('/', function (req, res) {
+router.delete('/', async function (req, res) {
     console.log("Deleting all tasks");
     tasks.splice(0, tasks.length);
     res.status(204).send();
 });
-
-
-
 
 
 module.exports = router;
