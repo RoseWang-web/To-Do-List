ebd114fe4db9386c6a2e09cfaef52f8ec0b27347
Piero Tinoco <tino4xc@gmail.com>
2023-07-14 20:32:29 -0600

    new changes

--- api/piero/tasks.js
+++ api/piero/tasks.js
@@ -81,6 +81,99 @@ router.get('/:id', function (req, res) {
     res.send(result[0]);
 });
 
+
+// POST /tasks
+router.post('/', function (req, res) {
+    console.log("Creating task");
+    const task = req.body;
+    if (!task.name) {
+        res.status(400).send({ message: "Task must have a name" });
+        return;
+    }
+
+    // Add the task
+    const newTask = {
+        id: new Date().getTime(),
+        name: task.name,
+        done: false,
+    };
+    tasks.push(newTask);
+    saveTasksToFile(tasks);
+    res.status(201).send(newTask);
+});
+
+// DELETE /tasks/:id
+router.delete('/:id', function (req, res) {
+    console.log("Handling request to delete a task");
+    const id = parseInt(req.params.id);
+    const index = tasks.findIndex((tasks) => tasks.id === id);
+    if (index === -1) {
+        res.status(404).send({ message: "Task not found" });
+        return;
+    }
+    tasks.splice(index, 1);
+    saveTasksToFile(tasks);
+    res.status(204).send();
+});
+function saveTasksToFile() {
+    fs.writeFileSync('tasks.json', JSON.stringify(tasks));
+}
+
+function loadTasksFromFile() {
+    if (!fs.existsSync('tasks.json')) {
+        return;
+    }
+    const json = fs.readFileSync('tasks.json');
+    tasks = JSON.parse(json);
+}
+
+loadTasksFromFile();
+
+// GET /tasks
+router.get('/', function (req, res) {
+    console.log("Handling request to search tasks");
+    res.send(tasks);
+});
+
+//Update a task based on its ID
+router.put('/:id', function (req, res) {
+    console.log("Handling request to update a task");
+    const id = parseInt(req.params.id);
+    const index = tasks.findIndex((tasks) => tasks.id === id);
+    if (index === -1) {
+        res.status(404).send({ message: "Not found" });
+        return;
+    }
+
+    const task = req.body;
+    const { name, done } = task;
+    console.log('in', task);
+    if (!name || !name.trim()) {
+        res.status(400).send({ message: "Task must have a name" });
+        return;
+    }
+    if (typeof done !== "boolean") {
+        res.status(400).send({ message: "Invalid task data" });
+        return;
+    }
+    tasks[index].done = done;
+    console.log('out', tasks[index]);
+    saveTasksToFile(tasks);
+    res.send(tasks[index]);
+});
+
+// GET /tasks/:id
+router.get('/:id', function (req, res) {
+    console.log("Handling request to search tasks");
+    const id = parseInt(req.params.id);
+    const result = tasks.find((tasks) => tasks.id === id);
+    if (!result) {
+        res.status(404).send({ message: "Not found" });
+        return;
+    }
+    res.send(result[0]);
+});
+
 //Understand the purpose of this endpoint
 
 // POST /tasks
@@ -117,24 +210,5 @@ router.delete('/:id', function (req, res) {
     res.status(204).send();
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-// Create tasks table if needed
-(async function () {
-    try {
-        const sql = `
-    CREATE TABLE IF NOT EXISTS tasks(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255),
-        done BOOLEAN
-    );
-`;
-        await mysqlQuery(sql);
-    } catch (error) {
-        console.log("Database not up")
-    }
-})();
 
-module.exports = router;
->>>>>>> c8d38012bb588121bc50bd9602ea1027edcb8d29
